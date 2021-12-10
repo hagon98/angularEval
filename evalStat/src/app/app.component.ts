ef4c266155243470a9ed675ab094e561a66bc82d
Diallo Souleymane <sasougen@gmail.com>
2021-12-10 12:43:52 +0100

    liste

--- evalStat/src/app/app.component.ts
+++ evalStat/src/app/app.component.ts
@@ -9,6 +9,7 @@ import { Statistique } from './models/statistique'
 export class AppComponent {
   title = 'evalStat'
   tabStat: Statistique[] = []
+  crossStat = new Statistique('deddcsdf-5545', 'Cross', '41M')
   constructor () {
     let statBasket = new Statistique(
       'beshfh-10-qdhgx',
@@ -16,6 +17,14 @@ export class AppComponent {
       '55M'
     )
     let statAthle = new Statistique('gdbec-789-xhhx-bdvsh', 'Athlétisme', '45M')
+    setTimeout(() => {
+      let statCross = new Statistique('fbdhs-789-fhfjfvb', 'Cross', '41M')
+    }, 2000)
+
+    setTimeout(() => {
+      this.crossStat
+      this.tabStat.push(this.crossStat)
+    }, 4000)
 
     this.tabStat.push(statBasket, statAthle)
   }
