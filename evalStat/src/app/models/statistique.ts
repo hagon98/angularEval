516ef82602d727153fbc753f9ec53b1608b74340
Diallo Souleymane <sasougen@gmail.com>
2021-12-10 11:16:57 +0100

    init classe statistique

--- evalStat/src/app/models/statistique.ts
+++ evalStat/src/app/models/statistique.ts
@@ -1,2 +1,11 @@
 export class Statistique {
+  unIdentifiant: string
+  unTitre: string
+  uneValeur: string
+
+  constructor (id: string, titre: string, valeur: string) {
+    this.unIdentifiant = id
+    this.unTitre = titre
+    this.uneValeur = valeur
+  }
 }
